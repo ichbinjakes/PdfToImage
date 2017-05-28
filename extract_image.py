18b8fddc501d62b053304499e562d64f4137e5f6
ichbinjakes <souter_downhill@hotmail.com>
2017-05-28 12:44:08 +1000

    Update extract_image.py

--- extract_image.py
+++ extract_image.py
@@ -1,15 +1,4 @@
 import os
-"""
-try:
-    os.mkdir("test")
-except:
-    print("Output directory already exists. Please delete, rename or move it before continuing.")
-"""
-#os.system("cp PdfToImage.class test")
-
-#
-
-#os.system("rm test/PdfToImage.class")
 
 def getExtension(string):
     if len(string.rsplit('.')) > 1:
@@ -30,24 +19,11 @@ for i in pfdFiles:
     os.system("java PdfToImage " + "\"" +i[0] + "." + i[1] + "\"")
 
 """
-try:
-    os.mkdir("test")
-except:
-    print("Output directory already exists. Please delete, rename or move it before continuing.")
-
-os.system("java PdfToImage \"test.jpg\"")
-"""
-
-"""
 -> object of pdfs
 -> split at pdf or take string to first .
 -> mkdir with that name
--> copy java .class to folder
 -> execute java
--> delete java .class
-
 -> multiprocess
-"""
-"""
+
 max theoretical filsize is probably around 2 Gb, this has not been tested.
 """
